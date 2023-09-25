e2b90a42471b3dac44e9a2d458cbfc34998b9f81
ed-favour <izebhijefavouredith@gmail.com>
2023-09-25 17:32:10 +0100

    dashboard commit

---
+++ client/src/components/DashBoardCard.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { FaBeer,LuLayers } from 'react-icons/fa';
+
+const DashBoardCard = () => {
+  return (
+    <div className="mx-5 bg-[#F8F9FC] p-10">
+      {/* <div className='grid gap-4 mb-8 mx-8 md:grid-cols-4 xl:grid-cols-4'> */}
+
+      <div className="flex flex-1 w-full h-full max-sm:w-full  ">
+        <div className="w-1/4 h-48 bg-slate-500 rounded-md">
+          <div className="m-10 p-5 justify-center items-center text-center">
+            <div className=""><FaBeer /> <LuLayers/>
+            </div>
+            <div>Todays order</div>
+            <div>$0.00</div>
+          </div>
+        </div>
+        <div className="w-1/4 h-48 ml-4 bg-orange-400 rounded-md">
+          <div className="m-10 p-5 justify-center items-center text-center">
+            <div className="">img</div>
+            <div>Todays order</div>
+            <div>$0.00</div>
+          </div>
+        </div>
+        <div className="w-1/4 h-48 ml-4 bg-red-400 rounded-md">
+          <div className="m-10 p-5 justify-center items-center text-center">
+            <div className="">img</div>
+            <div>Todays order</div>
+            <div>$0.00</div>
+          </div>
+        </div>
+        <div className="w-1/4 h-48 ml-4 bg-yellow-400 rounded-md">
+          <div className="m-10 p-5 justify-center items-center text-center">
+            <div className="">img</div>
+            <div>Todays order</div>
+            <div>$0.00</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-1 w-full h-full max-sm:w-full my-10 ">
+        <div className="w-1/4 h-28 mr-4 bg-white border border-solid border-gray-200 rounded-md flex  justify-center items-center text-center">
+          <div className="w-1/4">img</div>
+          <div>
+            Total Order <br /> 343
+          </div>
+        </div>
+
+        <div className="w-1/4 h-28 mr-4 bg-white border border-solid border-gray-200 rounded-md flex  justify-center items-center text-center">
+          <div className="w-1/4">img</div>
+          <div>
+            Orders Pending <br /> 96
+          </div>
+        </div>
+
+        <div className="w-1/4 h-28 mr-4 bg-white border border-solid border-gray-200 rounded-md flex  justify-center items-center text-center">
+          <div className="w-1/4">img</div>
+          <div>
+            Orders Processing <br />18
+          </div>
+        </div>
+
+        <div className="w-1/4 h-28 bg-white border border-solid border-gray-200 rounded-md flex  justify-center items-center text-center">
+          <div className="w-1/4">img</div>
+          <div>
+            Orders Delivered
+            <br /> 227
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoardCard;
--- client/src/pages/dashboard/dashboardhome/DashBoardHome.jsx
+++ client/src/pages/dashboard/dashboardhome/DashBoardHome.jsx
@@ -1,6 +1,14 @@
+import DashBoardCard from "../../../components/DashBoardCard"
 
 export const DashBoardHome = () => {
   return (
-    <div>DashBoardHome</div>
+    <div className="bg-[#F8F9FC]">
+  <div className="px-6 mx-auto bg-[#F8F9FC]">
+    <div className="flex items-center justify-between">
+    <h1 className="text-[#35155D] text-lg leading-[34px] font-normal cursor-pointer">Dashboard Overview</h1>
+    </div>
+  </div>   
+  <DashBoardCard/> 
+  </div>
   )
 }
